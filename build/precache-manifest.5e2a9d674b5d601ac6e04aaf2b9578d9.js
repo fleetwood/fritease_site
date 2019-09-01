81705f7ee557042f46f4fc9c79c69919c727237c
Fleetwood <wizening@gmail.com>
2019-09-01 19:40:06 -0400

    React working.

---
+++ build/precache-manifest.5e2a9d674b5d601ac6e04aaf2b9578d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba5f28ec1ecec6c78c4cb78885d4326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41ba5d6990f91b50001e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dcc39c785bba954ad379",
+    "url": "/static/js/2.b5e65b41.chunk.js"
+  },
+  {
+    "revision": "41ba5d6990f91b50001e",
+    "url": "/static/js/main.15f3c0cb.chunk.js"
+  },
+  {
+    "revision": "ad95ab8cf7eab4a28e81",
+    "url": "/static/js/runtime~main.813feab7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <p>#FriTease</p>
+  );
+}
+
+export default App;
